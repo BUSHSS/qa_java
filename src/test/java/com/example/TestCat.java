2010120cd135743b2b683433dabd2fc35ebe5599
Sergey <xoroshee@gmail.com>
2023-01-11 12:12:28 +0300

    Sprint 6 Base

--- src/test/java/com/example/TestCat.java
+++ src/test/java/com/example/TestCat.java
@@ -5,13 +5,6 @@ import org.junit.Test;
 
 public class TestCat {
 
-    Feline feline;
 
-    @Test
-//Проверка покрытия getSound в Cat
-    public void getSound() throws Exception {
-        Cat cat=new Cat(feline);
-        Assert.assertEquals("Meow", cat.getSound());
-    }
 
 }
